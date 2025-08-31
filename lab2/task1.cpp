7d39982bc34ab9cc4a4d7dd82eabcec10fd7cf23
seniya0610 <seniyanaeem@gmail.com>
2025-09-01 00:11:34 +0500

    Create task1.cpp

---
+++ lab2/task1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+    cout << "Enter size of array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = 0;
+    }
+
+    int index, value;
+    char choice;
+    do {
+        cout << "Enter index to update (0 to " << size - 1 << "): ";
+        cin >> index;
+        if (index >= 0 && index < size) {
+            cout << "Value: ";
+            cin >> value;
+            arr[index] = value;
+        } else {
+            cout << "Invalid index!" << endl;
+        }
+
+        cout << "Update another value? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "\nArray: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
