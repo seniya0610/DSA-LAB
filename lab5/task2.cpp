7dfa35687d76dec660186532644cd59f7748637b
seniya0610 <seniyanaeem@gmail.com>
2025-10-06 08:44:56 +0500

    Update task2.cpp

--- lab5/task2.cpp
+++ lab5/task2.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int indirectprint(int n);
+
 int printrecur(int n){
     if (n==0){
         return 0;
     }else{
-        printrecur(n-1);
+        indirectprint(n-1);
         cout<<n<<endl;
         return n;
     }
+}
+
+int indirectprint(int n){
+    return printrecur(n);
 }
 
 int main(){
