fd9a7bf4a2a54900fedd29d7f50e0ce4d7ffe0df
seniya0610 <seniyanaeem@gmail.com>
2025-12-01 06:30:24 +0500

    Update ReHashing.cpp

--- lab11/ReHashing.cpp
+++ lab11/ReHashing.cpp
@@ -1,53 +1,106 @@
 #include <iostream>
 using namespace std;
 
-class RehashTable {
+// Hashing with Linear Probing
+
+class Node
+{
+public:
+    int key;
+    int value;
+
+    Node(int k, int v) : key(k), value(v) {}
+    Node() : key(-1), value(-1) {} // empty slot
+};
+
+class LinearHash
+{
 public:
     int size;
-    int *table;
+    Node *table;
 
-    RehashTable(int s) {
-        size = s;
-        table = new int[size];
-        for (int i = 0; i < size; i++)
-            table[i] = -1;
+    LinearHash(int s) : size(s)
+    {
+        table = new Node[size];
     }
 
-    int hashFunction(int key) {
+    int hashFunction(int key)
+    {
         return key % size;
     }
 
-    // Insert using linear probing
-    void insert(int key) {
+    void insert(int key, int value)
+    {
         int index = hashFunction(key);
 
+        // find empty slot using linear probing
         int start = index;
-        while (table[index] != -1) {
+        while (table[index].key != -1)
+        {
             index = (index + 1) % size;
-            if (index == start) {
-                cout << "Table Full! Cannot insert\n";
+            if (index == start)
+            {
+                cout << "Hash Table Full! Cannot insert " << key << endl;
                 return;
             }
         }
-        table[index] = key;
+
+        table[index] = Node(key, value);
     }
 
-    // Count elements
-    float loadFactor() {
+    float loadFactor()
+    {
         int count = 0;
         for (int i = 0; i < size; i++)
-            if (table[i] != -1)
+            if (table[i].key != -1)
+            {
                 count++;
+            }
 
         return (float)count / size;
     }
 
-    // Returns next prime number after n
-    int nextPrime(int n) {
-        while (true) {
+    void display()
+    {
+        cout << "Hash Table (Linear Probing):";
+        for (int i = 0; i < size; i++)
+        {
+            if (table[i].key != -1)
+                cout << "Index " << i << " -> Key: " << table[i].key << ", Value: " << table[i].value << endl;
+            else
+                cout << "Index " << i << " -> Empty" << endl;
+        }
+    }
+
+    void deleteNode(int key)
+    {
+        int index = hashFunction(key);
+        int start = index;
+
+        while (table[index].key != -1)
+        {
+            if (table[index].key == key)
+            {
+                table[index] = Node(); // reset to empty
+                cout << "Deleted key " << key << endl;
+                return;
+            }
+            index = (index + 1) % size;
+            if (index == start)
+                break;
+        }
+        cout << "Key " << key << " not found!" << endl;
+    }
+
+    int nextPrime(int n)
+    {
+        while (true)
+        {
             bool isPrime = true;
-            for (int i = 2; i * i <= n; i++) {
-                if (n % i == 0) {
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
                     isPrime = false;
                     break;
                 }
@@ -58,53 +111,43 @@ public:
         }
     }
 
-    // Perform rehashing
-    void rehash() {
-        cout << "\nRehashing triggered!\n";
-
+    void rehash()
+    {
         int oldSize = size;
-        int *oldTable = table;
+        Node *oldTable = table;
 
-        size = nextPrime(size * 2);  // new size = prime after double
-        table = new int[size];
+        size = nextPrime(2 * oldSize);
+        table = new Node[size];
 
-        for (int i = 0; i < size; i++)
-            table[i] = -1;
-
-        cout << "New table size: " << size << endl;
-
-        for (int i = 0; i < oldSize; i++) {
-            if (oldTable[i] != -1)
-                insert(oldTable[i]);
+        for (int i = 0; i < oldSize; i++)
+        {
+            if (oldTable[i].key != -1)
+            {
+                insert(oldTable[i].key, oldTable[i].value);
+            }
         }
-    }
 
-    void display() {
-        cout << "\nHash Table:\n";
-        for (int i = 0; i < size; i++) {
-            if (table[i] != -1)
-                cout << i << ": " << table[i] << endl;
-            else
-                cout << i << ": Empty\n";
-        }
+        delete[] oldTable;
     }
 };
 
-int main() {
-    RehashTable ht(7);
+int main()
+{
+    LinearHash hashTable(5);
 
-    int keys[] = {10, 20, 5, 15, 7, 32, 14, 28};
-    int n = 8;
+    hashTable.insert(1, 100);
+    hashTable.insert(6, 200);  // Should collide with index 1
+    hashTable.insert(11, 300); // Should collide with index 1
+    hashTable.insert(2, 400);
+    hashTable.insert(3, 500);
 
-    for (int i = 0; i < n; i++) {
-        cout << "\nInserting: " << keys[i];
-        ht.insert(keys[i]);
+    hashTable.display();
 
-        if (ht.loadFactor() > 0.7)
-            ht.rehash();
-    }
+    cout << "Load Factor: " << hashTable.loadFactor() << endl;
 
-    ht.display();
+    cout << "\nRehashing...\n" << endl;
+    hashTable.rehash();
+    hashTable.display();
 
     return 0;
 }
